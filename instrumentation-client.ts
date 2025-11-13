6b291f5424f304eacd9591af2396496c671da060
Jason Desiderio <jason@thegoodfornothings.club>
2025-11-12 23:18:43 -0600

    Try the simplified config

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,6 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: '/ingest',
-  ui_host: 'https://us.i.posthog.com',
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
   defaults: '2025-05-24',
-  capture_exceptions: true, // enables Error Tracking
-  debug: process.env.NODE_ENV === 'development',
 })
--- next.config.ts
+++ next.config.ts
@@ -5,20 +5,6 @@ const nextConfig: NextConfig = {
   cacheComponents: false,
   reactCompiler: true,
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*',
-      },
-    ]
-  },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
 }
 
 export default withPostHogConfig(nextConfig, {
