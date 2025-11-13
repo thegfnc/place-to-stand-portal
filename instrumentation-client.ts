27005d53b5281c0e34a783989f5d70bbd5be422e
Jason Desiderio <jason@thegoodfornothings.club>
2025-11-12 23:32:15 -0600

    Add proxy

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,6 +1,13 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+  api_host: '/relay-HVAq/',
+  ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
   defaults: '2025-05-24',
+  capture_exceptions: true,
+  capture_heatmaps: true,
+  capture_dead_clicks: true,
+  capture_pageleave: true,
+  capture_pageview: true,
+  capture_performance: true,
 })
