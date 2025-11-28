f5c653e82c811fc0e23b77acf0988c8cf6009a1f
Jason Desiderio <jason@thegoodfornothings.club>
2025-11-28 14:47:15 -0600

    fix: Change sidebar vertical overflow behavior from scroll to auto.

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -130,7 +130,7 @@ export function Sidebar({ user }: Props) {
   }, [theme, themeMounted])
 
   return (
-    <aside className='bg-background/90 hidden h-screen w-72 shrink-0 overflow-y-scroll border-r md:flex md:flex-col'>
+    <aside className='bg-background/90 hidden h-screen w-72 shrink-0 overflow-y-auto border-r md:flex md:flex-col'>
       <div className='flex flex-1 flex-col'>
         <div className='space-y-10 px-6 py-8'>
           <div suppressHydrationWarning>
